c264c62582f873e7284c17b219e35bc4ce454361
BinWang02 <qq1678832865@gmail.com>
2025-10-22 12:07:15 -0400

    add link

--- main.rb
+++ main.rb
@@ -2,9 +2,20 @@
 # Set this to my project directory
 PROJECT_DIR = "home/BingWang/Sonic-Pi-Music-Creation"
 
-valley = PROJECT_DIR + "/samples/wave/a-ride-through-chinese-valley-62241.wav"
-beat   = PROJECT_DIR + "/samples/wave/chinese-beat-190047.wav"
-solo   = PROJECT_DIR + "/samples/wave/guzheng-solo-22676.wav"
-bowed  = PROJECT_DIR + "/samples/wave/lovely-chinese-guzheng-bowed-16700.wav"
 
-sample valley
+# Define sample file paths
+# Guzheng samples
+
+# https://pixabay.com/sound-effects/a-ride-through-chinese-valley-62241/
+valleyGuzheng = PROJECT_DIR + "/samples/wave/a-ride-through-chinese-valley-62241.wav"
+# https://pixabay.com/sound-effects/chinese-beat-190047/
+
+beatDrum   = PROJECT_DIR + "/samples/wave/chinese-beat-190047.wav"
+# https://pixabay.com/sound-effects/guzheng-solo-22676/
+
+soloGuzheng   = PROJECT_DIR + "/samples/wave/guzheng-solo-22676.wav"
+# https://pixabay.com/sound-effects/lovely-chinese-guzheng-bowed-16700/
+
+bowedGuzheng  = PROJECT_DIR + "/samples/wave/lovely-chinese-guzheng-bowed-16700.wav"
+
+sample valleyGuzheng
